20d567b2a4611c65bab39301520c93a3b7095db5
andy16777216 <ajscacco@ucdavis.edu>
2014-11-14 16:26:50 -0800

    Update andynewv2.py

--- cosmoslik/andynewv2.py
+++ cosmoslik/andynewv2.py
@@ -85,7 +85,7 @@ class main(SlikPlugin):
 	#print 'loading sampler'
         self.sampler = get_plugin('samplers.metropolis_hastings')(
              self,
-             num_samples=1000000,
+             num_samples=100,
              output_file='chains/andynewv1.chain',
              proposal_cov='../data/proposal.covmat',
              proposal_scale=1,
