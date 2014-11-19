a248fed2aaa6c5ef84d6e78648bd46d3d04e938a
andy16777216 <ajscacco@ucdavis.edu>
2014-11-19 14:20:56 -0800

    Update classy.py

--- cosmoslik/cosmoslik_plugins/models/classy.py
+++ cosmoslik/cosmoslik_plugins/models/classy.py
@@ -27,7 +27,7 @@ class classy(SlikPlugin):
                     'pivot_scalar':'k_pivot'
                     #'Tcmb':'T_cmb',
                     #'P_k_max_hinvMpc':'P_k_max_h/Mpc'
-                    #'w':'w',
+                    'w':'w',
                     #'nrun':'alpha_s',
                     #'omk':'Omega_k',
                     #'l_max_scalar':'l_max_scalars',
