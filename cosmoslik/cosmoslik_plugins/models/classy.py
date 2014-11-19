d6b50c96ca94d2e19708e01135d7a442f0975c3a
andy16777216 <ajscacco@ucdavis.edu>
2014-11-19 14:06:31 -0800

    Update classy.py

--- cosmoslik/cosmoslik_plugins/models/classy.py
+++ cosmoslik/cosmoslik_plugins/models/classy.py
@@ -24,15 +24,15 @@ class classy(SlikPlugin):
                     'massive_neutrinos':'N_ncdm',
                     'massless_neutrinos':'N_ur',
                     'Yp':'YHe',
-                    'Tcmb':'T_cmb',
+                    #'Tcmb':'T_cmb',
                     'pivot_scalar':'k_pivot',
                     #'P_k_max_hinvMpc':'P_k_max_h/Mpc'
                     #'w':'w0_fld',
                     #'r':'r',
                     #'nrun':'alpha_s',
-                    'omk':'Omega_k',
-                    'l_max_scalar':'l_max_scalars',
-                    'l_max_tensor':'l_max_tensors'
+                    #'omk':'Omega_k',
+                    #'l_max_scalar':'l_max_scalars',
+                    #'l_max_tensor':'l_max_tensors'
                     }
 
 
@@ -56,8 +56,8 @@ class classy(SlikPlugin):
                  k_c,
                  alpha_exp,
                  tau,
-                 #omnuh2=0, #0.006
-                 w=None, #None means that Class will take the default for this, maybe?
+                 #omnuh2=0, #0.006  #None means that Class will take the default for this, maybe?
+                 w=None,
                  r=None,
                  nrun=None,
                  omk=0,
