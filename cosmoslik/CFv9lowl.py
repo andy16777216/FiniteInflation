4cec6a3983fd910bfee8c9249d4b8872f9356e21
andy16777216 <ajscacco@ucdavis.edu>
2014-12-12 15:01:06 -0800

    Update CFv9lowl.py

--- cosmoslik/CFv9lowl.py
+++ cosmoslik/CFv9lowl.py
@@ -113,7 +113,7 @@ class main(SlikPlugin):
              proposal_cov='../data/proposal.covmat',
              proposal_scale=1,
              #print_level=0,
-             output_extra_params=['cosmo.Yp','cosmo.H0','cosmo.kcactual','cosmo.alphaactual','cl_TT2','cl_TT3','cl_TT4','cl_TT5','cl_TT6','cl_TT7','cl_TT8','cl_TT20','cl_TT40','cl_TT80','cl_TT120','cl_TT200','cl_TT500','cl_TT1000','cl_TT2000','cl_TT2999']
+             output_extra_params=['cosmo.Yp','cosmo.H0','cosmo.kcactual','cosmo.alphaactual','cl_TT2','cl_TT3','cl_TT4','cl_TT5','cl_TT6','cl_TT7','cl_TT8','cl_TT20','cl_TT40','cl_TT80','cl_TT120','cl_TT200','cl_TT500']
 	)
 
 
@@ -142,9 +142,6 @@ class main(SlikPlugin):
         self.cl_TT120 = self.cl_TT[120]
         self.cl_TT200 = self.cl_TT[200]
         self.cl_TT500 = self.cl_TT[500]
-        self.cl_TT1000 = self.cl_TT[1000]
-        self.cl_TT2000 = self.cl_TT[2000]
-        self.cl_TT2999 = self.cl_TT[2999]
         
         return lsum(lambda: self.priors(self),
                     lambda: self.camspec(self.cmb_result),
