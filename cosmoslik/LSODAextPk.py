4af1934d81859757614d377ab068d9ea55923bbc
andy16777216 <ajscacco@ucdavis.edu>
2014-12-17 17:32:38 -0800

    Update LSODAextPk.py

--- cosmoslik/LSODAextPk.py
+++ cosmoslik/LSODAextPk.py
@@ -118,7 +118,7 @@ class main(SlikPlugin):
 	#print 'loading sampler'
         self.sampler = get_plugin('samplers.metropolis_hastings')(
              self,
-             num_samples=2,
+             num_samples=100,
              output_file='chains/LSODA1.chain',
              proposal_cov='../data/proposal.covmat',
              proposal_scale=1,
