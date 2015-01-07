f44f92f3138e6ff7a67603c1d5adb756938922d0
andy16777216 <ajscacco@ucdavis.edu>
2015-01-07 14:35:10 -0800

    Update classy.py

--- cosmoslik/cosmoslik_plugins/models/classy.py
+++ cosmoslik/cosmoslik_plugins/models/classy.py
@@ -30,7 +30,7 @@ class classy(SlikPlugin):
                     #'nrun':'alpha_s',
                     #'omk':'Omega_k',
                     #'l_max_scalar':'l_max_scalars',
-                    'l_max_tensor':'l_max_tensors',
+                    #'l_max_tensor':'l_max_tensors',
                     'phi0':'custom1',
                     'm6':'custom2',
                     'modes':'modes',
@@ -82,7 +82,7 @@ class classy(SlikPlugin):
         self.model.set(output='tCl, lCl, pCl',
                        #lensing='yes',
                        l_max_scalars=l_max_scalar,
-                       #l_max_tensors=l_max_tensor,
+                       l_max_tensors=l_max_tensor,
                        command = '../LSODAtesnors/pk',
                        #custom1 = phi0
                        #custom2 = m6,
