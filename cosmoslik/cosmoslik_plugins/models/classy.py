7ec610d8556ec083968e863d18ecd5ad86eae9e7
andy16777216 <ajscacco@ucdavis.edu>
2015-01-07 14:16:12 -0800

    Update classy.py

--- cosmoslik/cosmoslik_plugins/models/classy.py
+++ cosmoslik/cosmoslik_plugins/models/classy.py
@@ -80,8 +80,8 @@ class classy(SlikPlugin):
                           if k in self.name_mapping and v is not None}
         d['P_k_ini type']='external_Pk'
         self.model.set(output='tCl, lCl, pCl',
-                       lensing='yes',
-                       l_max_scalars=l_max_scalar,
+                       #lensing='yes',
+                       #l_max_scalars=l_max_scalar,
                        command = '../LSODAtesnors/pk',
                        #custom1 = phi0
                        #custom2 = m6,
