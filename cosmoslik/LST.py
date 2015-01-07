6520986246090e93e2c2595c0198c855db5459ab
andy16777216 <ajscacco@ucdavis.edu>
2015-01-07 14:34:19 -0800

    Update LST.py

--- cosmoslik/LST.py
+++ cosmoslik/LST.py
@@ -37,7 +37,7 @@ class main(SlikPlugin):
             l_max_tensor=3000,
             pivot_scalar=0.05,
             w=-1.0,
-            r=None,
+            #r=None,
             nrun=None,
             omk=0,
             Yp=None,
