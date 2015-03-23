c33b0d5e4170f7f11f3cb68406d8f9c2b2508973
andy16777216 <ajscacco@ucdavis.edu>
2015-03-23 16:11:36 -0700

    Update LSTfiniteR2.py

--- cosmoslik/LSTfiniteR2.py
+++ cosmoslik/LSTfiniteR2.py
@@ -80,7 +80,7 @@ class main(SlikPlugin):
 	#print 'loading sampler'
         self.sampler = get_plugin('samplers.metropolis_hastings')(
              self,
-             num_samples=100,
+             num_samples=1000000,
              output_file='chains/LSTfiniteR2.chain',
              proposal_cov='../data/proposal.covmat',
              proposal_scale=1,
