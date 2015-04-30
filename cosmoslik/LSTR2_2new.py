395350753afc0d84bae2d1cdbcfbabca41e42b91
andy16777216 <ajscacco@ucdavis.edu>
2015-04-30 13:10:37 -0700

    Update LSTR2_2new.py

--- cosmoslik/LSTR2_2new.py
+++ cosmoslik/LSTR2_2new.py
@@ -19,12 +19,14 @@ class main(SlikPlugin):
         d['output']='tCl, lCl, pCl'
         d['command'] = '../LSTfiniteR2/pk'
         d['tau'] = param(0.09, range=(0.05,0.15))
+        d['100*theta_s'] = param(1.0413)
+        d['theta'] = 0.01*d['100*theta_s']
 
         self.cosmo = get_plugin('models.cosmology')(
             ombh2 = param(0.0221),
             omch2 = param(0.12),
             #tau = param(0.09, range=(0.05,0.15)),
-            theta = param(0.010413),
+            #theta = param(0.010413),
             phi0 = param(9.8, scale = 2.4, range = (9.2, 10.0)),
             L = param(3.5, scale = 2.4, range = (2.5, 4.5)),
             logkc = param(8.4, scale = 2.4, range = (6.6, 10.4)),
